f4ef5a2c96cfa5370d92fc6886dc26861bea3885
AlvaroJimenezMedina <alvaro.jimenez.medina.84@gmail.com>
2021-03-15 20:26:59 +0100

    Cambio en el método getArea, usando 'potencia'

--- project4/Circle.java
+++ project4/Circle.java
@@ -1,4 +1,3 @@
-
 import java.io.PrintWriter;
 /**
  * This is a circle class which is representing radius and center of circle. 
@@ -7,79 +6,62 @@ import java.io.PrintWriter;
  * @author  Pratik Patel
  * @version 1 Date: 04/27/2014
  */
-
 public class Circle  extends TwoDimensionalShape{
     //instance variables radius and center
-    private double  radius ;
+    private double radius ;
     private Point center;
 
     /**
      * The default constructor which takes doubles and point initializes the radius and center. 
      */
-    public Circle( Point center,double radius) 
-    {
+    public Circle(Point center,double radius) {
         this.radius = radius;
         this.center = center;
     }
-    
     
     /**
      * This constructor is same as define above. 
      */
-    public Circle(Circle circle)
-    {
+    public Circle(Circle circle) {
         this.radius = circle.radius;
         this.center = circle.center;
     }
 
-    
     /**
      * It gets the value of radius.
      * @return the value of radius.
      */
-    public double getRadius() 
-    {
+    public double getRadius() {
         return radius;
     }
 
-    
-    
     /**
      * It sets the value of radius.
      */
-    public void setRadius(double radius) 
-    {
+    public void setRadius(double radius) {
         this.radius = radius;
     }
 
-    
     /**
      * It gets the value of center.
      * @return the value of center.
      */
-    public Point getCenter() 
-    {
+    public Point getCenter() {
         return center;
     }
-
-    
     
     /**
      * It sets the value of center.
      */
-    public void setCenter(Point center) 
-    {
+    public void setCenter(Point center) {
         this.center = center;
     }
 
-    
-    
     /**
      * This is a printwriteout overridden method which prints the circle object.
      */
     @Override
-    public void print(PrintWriter out) 
-    {
+    public void print(PrintWriter out) {
         super.print(out);
         out.write("\n Cirle: "+ "\r\n");
         out.print("\n Center = " + this.center.getX() + " " + this.center.getY()+ "\r\n");
@@ -89,17 +71,14 @@ public class Circle  extends TwoDimensionalShape{
         out.write("\n\n\n");
     }
 
-    
     /**
      * This is an overridden method which gets the area.
      */
     @Override
-    public double getArea() 
-    {
-        return Math.PI * this.radius * this.radius ;
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
     }
 
-    
     /**
      * This is an overridden method which gets the radius.
      */
@@ -107,10 +86,7 @@ public class Circle  extends TwoDimensionalShape{
     public double getPerimeter() {
         return 2 * Math.PI * this.radius;
     }
-    
-    
-    
-    
+
 }
 
     
